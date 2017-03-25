c2696f3ca3fdcca41a6227a21cba56e0c67815b0
Chad Ostrowski <hi@chadoh.com>
2017-03-24 22:20:53 -0400

    Let's get definitive
    Remove the 'maybe'

--- aws-upload.conf.js
+++ aws-upload.conf.js
@@ -1,7 +1,7 @@
 // currently running this from the 'build' dir, with ./deploy script
 module.exports = {
   credentials: "../aws-credentials.json",
-  bucketName: "maybe.chadoh.com",
+  bucketName: "chadoh.com",
   patterns:[
     "**/*",
   ]
