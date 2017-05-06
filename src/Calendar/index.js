6717b1d7f4f7cad4986b9487af822650979ee90a
Chad Ostrowski <hi@chadoh.com>
2017-05-06 19:43:36 -0400

    Get iFrame to adjust height on navigation
    This relies on some `postMessage` fanciness fired by the hosted
    Entire.Life calendar.
    
    At this point, I feel confident providing people some embed code in the
    Entire.Life UI. It looks pretty good.

--- src/Calendar/index.js
+++ src/Calendar/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './Calendar.css';
 
+function adjustHeight(message) {
+  var origin = message.origin;
+  var event = message.data.event;
+  var height = message.data.height;
+  if (origin === 'https://entire.life' && event === 'ENTIRELIFE_ROUTE_CHANGE') {
+    var iframe = document.querySelector('#entire-life-calendar');
+    iframe.height = '' + (height + 5) + 'px';
+  }
+}
+
+window.addEventListener('message', adjustHeight, false);
+
 export default () => (
   <div className="Calendar">
     <iframe
+      id="entire-life-calendar"
       src="https://entire.life/chadoh?embed"
       width="100%"
-      height="3350px"
+      height="1000px"
       frameBorder="0"
-      style={{border: 0}}
+      style={{
+        border: 0,
+        backgroundImage: 'url(https://cl.ly/kLqc/download/loading.svg)',
+        backgroundPosition: '50% 2em',
+        backgroundSize: '150px',
+        backgroundRepeat: 'no-repeat',
+      }}
     >
     </iframe>
   </div>
